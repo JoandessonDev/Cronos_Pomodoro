34f7b026ada30acef6483b465afd33d617a9c2bf
Joandesson <joandesson.dev@gmail.com>
2025-09-03 00:02:46 -0300

    creating new layout for new elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+import "./styles/global.css";
+import "./styles/theme.css";
+import { Heading } from "./assets/components/Heading";
+import { TimerIcon } from "lucide-react";
+import { Container } from "./assets/components/Container";
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    <Container>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores alias magni reiciendis dignissimos aperiam unde reprehenderit numquam ad nulla, illo et recusandae aut! Culpa exercitationem explicabo sapiente voluptatum harum architecto!</p>
+    </Container>
+  )
 }
-
-export default App;
