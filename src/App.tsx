2f4df6ba2dc6137f00011f875e8ac045d6e0c2af
Joandesson <joandesson.dev@gmail.com>
2025-09-08 08:12:11 -0300

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./styles/global.css";
 import "./styles/theme.css";
-import { Heading } from "./assets/components/Heading";
-import { TimerIcon } from "lucide-react";
 import { Container } from "./assets/components/Container";
 import { Logo } from "./assets/components/Logo";
 import { Menu } from "./assets/components/Menu";
 import { CountDown } from "./assets/components/CountDown";
 import { DefaultInput } from "./assets/components/DefaultInput";
+import { Cycles } from "./assets/components/Cycles";
+import { DefaultButton } from "./assets/components/DefaultButton";
+import { PlayCircleIcon } from "lucide-react";
+
 export function App() {
   return (
     <Container>
@@ -17,7 +19,7 @@ export function App() {
         <div className="formRow">
           <DefaultInput
             id="myInput"
-            disabled  
+            disabled
             labelText="Task"
             placeholder="Digite o nome da tarefa"
             type="text"
@@ -27,9 +29,12 @@ export function App() {
           <p>asdfa dfasfasfasf asdf asdf asdf sdfa sdf </p>
         </div>
         <div className="formRow">
-          <p>Ciclos</p>
-          <p>0 0 0 0 0 0 0 0</p>
+          <Cycles />
         </div>
+        <div className="formRow">
+          <DefaultButton icon={<PlayCircleIcon/>} />
+        </div>
+
         <div className="formRow">
           <button>enviar</button>
         </div>
