3f01898019564b168b5e9ba37f0c496dc308d3a7
Joandesson <joandesson.dev@gmail.com>
2025-09-26 09:37:28 -0400

    feat: testando uso de reducer

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { act, useEffect, useReducer, useState } from "react";
 import { initialTaskState } from "./InitialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -10,9 +10,28 @@ export function TaskContextProvider({children}: TaskContextProviderProps){
     useEffect(() => {
         console.log(state);
     }, [state])
+    // type ActionType = {
+    //     type: string;
+    //     payload?: number;
+    // }
+    // const [myState, dispatch] = useReducer((state, action: ActionType) => {
+    //     switch(action.type){
+    //         case 'incrementar': {
+    //             if (!action.payload) return;
+    //             return {
+    //                 ...state,
+    //                  secondsRemaing: state.secondsRemaing + action.payload }
+    //         }
+    //     }
+    //     return state;
+    // }, {
+    //     secondsRemaing: 0,
+    // })
     return(
         <TaskContext.Provider value={{state, setState}}>
             {children}
+            {/* <h1>O estado é {JSON.stringify(myState)}</h1>
+            <button onClick={() => {dispatch({type:'incrementar', payload: 10})}}>increment 10</button> */}
         </TaskContext.Provider>
     );
 }
